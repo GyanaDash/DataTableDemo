a7897aed723cc15dc4c1c7f47b6b0ab15ca45aeb
GYAN DASH <gyanaranjan.dash5@gmail.com>
2021-02-15 01:14:37 +0530

    Students Controller is now ready to handle the HTTP requests...

--- DataTableDemo/Controllers/StudentsController.cs
+++ DataTableDemo/Controllers/StudentsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataTableDemo.Models;
+using DataTableDemo.Service;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,40 +10,57 @@ using System.Threading.Tasks;
 
 namespace DataTableDemo.Controllers
 {
+
     [Route("api/[controller]")]
     [ApiController]
     public class StudentsController : ControllerBase
     {
+        
+        IStudentService _studentService = null;
+        public StudentsController(IStudentService studentService)
+        {
+            _studentService = studentService;
+        }
+
         // GET: api/<StudentsController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<Student> GetStudents()
         {
-            return new string[] { "value1", "value2" };
+            return _studentService.GetStudents();
         }
 
         // GET api/<StudentsController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public Student GetById(int id)
         {
-            return "value";
+            return _studentService.GetById(id);
         }
 
         // POST api/<StudentsController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void SaveOrUpdate([FromForm] Student student)
         {
+            if(student.StudentId == 0)
+            {
+                _studentService.Save(student);
+            }
+            else
+            {
+                _studentService.Update(student);
+            }
         }
 
-        // PUT api/<StudentsController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
 
         // DELETE api/<StudentsController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public string Delete(int id)
         {
+            return _studentService.Delete(id);
         }
     }
 }
